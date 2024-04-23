c1016298c10a8276962f71aa6c55de66d8f11668
SRINADH <115113319+Srinadhch07@users.noreply.github.com>
2024-04-23 07:28:54 +0530

    Create permutations.py

---
+++ permutations.py
@@ -0,0 +1,124 @@
+'''
+itertools.permutations(iterable[, r])
+
+This tool returns successive  length permutations of elements in an iterable.
+
+If  is not specified or is None, then  defaults to the length of the iterable, and all possible full length permutations are generated.
+
+Permutations are printed in a lexicographic sorted order. So, if the input iterable is sorted, the permutation tuples will be produced in a sorted order.
+
+Sample Code
+
+>>> from itertools import permutations
+>>> print permutations(['1','2','3'])
+<itertools.permutations object at 0x02A45210>
+>>> 
+>>> print list(permutations(['1','2','3']))
+[('1', '2', '3'), ('1', '3', '2'), ('2', '1', '3'), ('2', '3', '1'), ('3', '1', '2'), ('3', '2', '1')]
+>>> 
+>>> print list(permutations(['1','2','3'],2))
+[('1', '2'), ('1', '3'), ('2', '1'), ('2', '3'), ('3', '1'), ('3', '2')]
+>>>
+>>> print list(permutations('abc',3))
+[('a', 'b', 'c'), ('a', 'c', 'b'), ('b', 'a', 'c'), ('b', 'c', 'a'), ('c', 'a', 'b'), ('c', 'b', 'a')]
+Task
+
+You are given a string .
+Your task is to print all possible permutations of size  of the string in lexicographic sorted order.
+
+Input Format
+
+A single line containing the space separated string  and the integer value .
+
+Constraints
+
+
+The string contains only UPPERCASE characters.
+
+Output Format
+
+Print the permutations of the string  on separate lines.
+
+Sample Input
+
+HACK 2
+Sample Output
+
+AC
+AH
+AK
+CA
+CH
+CK
+HA
+HC
+HK
+KA
+KC
+KH
+Explanation
+
+All possible size  permutations of the string "HACK" are printed in lexicographic sorted order.
+
+Language
+Pypy 3
+More
+123456789
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import permutations
+values=input().split()
+final=list(permutations(sorted(values[0]),int(values[1])))
+for i in final:
+    print(''.join(i))
+
+
+
+Line: 6 Col: 20
+
+Test against custom input
+Python
+You have earned 10.00 points!
+You are now 5 points away from the gold level for your python badge.
+97%395/400
+Congratulations
+You solved this challenge. Would you like to challenge your friends?Share on FacebookShare on TwitterShare on LinkedIn
+
+Test case 0
+
+Test case 1
+
+Test case 2
+
+Test case 3
+
+Test case 4
+
+Test case 5
+Compiler Message
+Success
+Input (stdin)
+HACK 2
+Expected Output
+AC
+AH
+AK
+CA
+CH
+CK
+HA
+HC
+HK
+KA
+KC
+KH
+
+'''
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import permutations
+values=input().split()
+final=list(permutations(sorted(values[0]),int(values[1])))
+for i in final:
+    print(''.join(i))
+
+
