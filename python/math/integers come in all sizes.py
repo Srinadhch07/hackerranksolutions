5aac0428887d31c457bf6cdc3c91e20a7b89ffe5
SRINADH <115113319+Srinadhch07@users.noreply.github.com>
2024-06-11 00:43:25 +0530

    Create integers come in all sizes.py

---
+++ python/math/integers come in all sizes.py
@@ -0,0 +1,76 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+print(pow(a,b)+pow(c,d))
+
+'''
+ntegers in Python can be as big as the bytes in your machine's memory. There is no limit in size as there is:  (c++ int) or  (C++ long long int).
+
+As we know, the result of  grows really fast with increasing .
+
+Let's do some calculations on very large integers.
+
+Task
+Read four numbers, , , , and , and print the result of .
+
+Input Format
+Integers , , , and  are given on four separate lines, respectively.
+
+Constraints
+
+
+
+
+Output Format
+Print the result of  on one line.
+
+Sample Input
+
+9
+29
+7
+27
+Sample Output
+
+4710194409608608369201743232  
+Note: This result is bigger than . Hence, it won't fit in the long long int of C++ or a 64-bit integer.
+
+Language
+Python 3
+More
+123456
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+print(pow(a,b)+pow(c,d))
+
+
+
+Line: 6 Col: 25
+
+Test against custom input
+Python
+You have earned 10.00 points!
+57/115 challenges solved.
+50%
+Congratulations
+You solved this challenge. Would you like to challenge your friends?Share on FacebookShare on TwitterShare on LinkedIn
+
+Test case 0
+
+Test case 1
+Compiler Message
+Success
+Input (stdin)
+9
+29
+7
+27
+Expected Output
+4710194409608608369201743232
+
+'''
